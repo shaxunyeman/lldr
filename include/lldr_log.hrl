a28ba412bbe130fdb15e2831cf2029fad54ea5c3
db liu <shaxunyeman@gmail.com>
2014-06-08 19:35:30 +0800

    adding log's unit for lldr

--- include/lldr_log.hrl
+++ include/lldr_log.hrl
@@ -1,14 +1,19 @@
-%%
-%%
-%%
-
-%% LOG_LEVEL
--define(LOG_ERR,0).
--define(LOG_WARNING,1).
--define(LOG_INFO,2).
--define(LOG_DEBUG,3).
-
--define(ERROR_LOG,error_logger:error_msg).
--define(ERROR_WARING,erro_logger:warning_msg).
--define(ERROR_INFO,erro_logger:info_msg).
--define(ERROR_DEBUG,erro_logger:info_report).
+%% --------------------------------------------------------------------
+%% Logging mechanism
+%% Print in standard output
+-define(PRINT(Text),
+		io:format(Text)).
+-define(PRINT(Format, Args),
+		io:format(Format, Args)).
+-define(TEST_MSG(Format, Args),
+		logger:test_msg(?MODULE,?LINE,Format, Args)).
+-define(DEBUG(Format, Args),
+		logger:debug_msg(?MODULE,?LINE,Format, Args)).
+-define(INFO_MSG(Format, Args),
+		logger:info_msg(?MODULE,?LINE,Format, Args)).
+-define(WARNING_MSG(Format, Args),
+		logger:warning_msg(?MODULE,?LINE,Format, Args)).
+-define(ERROR_MSG(Format, Args),
+		logger:error_msg(?MODULE,?LINE,Format, Args)).
+-define(CRITICAL_MSG(Format, Args),
+		logger:critical_msg(?MODULE,?LINE,Format, Args)).
